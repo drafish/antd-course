78636f832d8f0fb3e6684686fbf9982eca2aafb9
xwlyy <xwlyy1991@163.com>
2019-05-20 14:34:53 +0800

    路由配置

--- config/config.js
+++ config/config.js
@@ -8,9 +8,23 @@ export default {
   routes: [{
     path: '/',
     component: '../layout',
-    routes: [{
-      path: 'helloworld',
-      component: './HelloWorld',
-    }]
+    routes: [
+      {
+        path: '/',
+        component: './HelloWorld',
+      },
+      {
+        path: '/helloworld',
+        component: './HelloWorld',
+      },
+      {
+        path: '/dashboard',
+        routes: [
+          { path: '/dashboard/analysis', component: 'Dashboard/Analysis' },
+          { path: '/dashboard/monitor', component: 'Dashboard/Monitor' },
+          { path: '/dashboard/workplace', component: 'Dashboard/Workplace' }
+        ],
+      }
+    ]
   }]
 };
